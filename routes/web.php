d1d1aeee2003c5c57d22ff7ee14b96ed6df733df
deepakagarwal1995 <37587832+deepakagarwal1995@users.noreply.github.com>
2024-03-28 17:41:28 +0530

    migrate

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,22 @@ Route::resource('delivery', OrderDeliveryController::class);
 Route::get('/delivery/{id}/create', [OrderDeliveryController::class, 'create'])->name('delivery.create');
 Route::get('/delivery/{id}/view', [OrderDeliveryController::class, 'view'])->name('delivery.view');
 Route::resource('user', UserController::class);
+
+
+
+
+Route::get('migrate', function () {
+    Artisan::call('view:clear');
+    Artisan::call('route:clear');
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('migrate');
+});
+
+
+Route::get('/storage-link', function () {
+    $target = storage_path('app/public');
+    $link = public_path('/storage');
+    echo symlink($target, $link);
+    // echo "symbolic link created successfully";
+});
